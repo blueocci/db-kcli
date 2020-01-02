3b645f89256ee07cfc346bf212eb506308ccb52f
hejack0207 <hejack0207@sina.com>
2020-01-02 09:42:42 +0800

    fix MysqlLinter.kt

--- src/main/kotlin/mysql/MysqlLinter.kt
+++ src/main/kotlin/mysql/MysqlLinter.kt
@@ -1,11 +1,31 @@
 package mysql
 
 import me.liuwj.ktorm.database.Database
+import me.liuwj.ktorm.dsl.iterable
 import me.liuwj.ktorm.dsl.select
 import sso.Users
 
-fun main() {
-    Database.connect("jdbc:mysql://localhost:3306/ktorm", driver = "com.mysql.jdbc.Driver")
+fun startlint(server:String,port:Int,dbname:String) {
+    val db = Database.connect("jdbc:mysql://$server:$port/$dbname", driver = "com.mysql.jdbc.Driver")
+
+    val names = db.useConnection { conn ->
+        val sql = """
+        select name from t_employee
+        where department_id = ?
+        order by id
+    """
+
+        conn.prepareStatement(sql).use { statement ->
+            statement.setInt(1, 1)
+            statement.executeQuery().iterable().map {
+                it.getString(1)
+            }
+        }
+    }
+
+    names.forEach {
+        println(it)
+    }
 }
 
 class UserCommand {
